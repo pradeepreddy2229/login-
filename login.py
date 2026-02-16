aec82edaecba7930be13b8776ad3a1c270442a1f
pradeepreddy2229 <kundurupradeep56@gmail.com>
2026-02-16 12:47:19 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bus Booking</title>
+</head>
+<body>
+
+<h1>Bus Booking Form</h1>
+
+<form>
+
+Name:<br>
+<input type="text"><br><br>
+
+From:<br>
+<input type="text"><br><br>
+
+To:<br>
+<input type="text"><br><br>
+
+Date:<br>
+<input type="date"><br><br>
+
+Number of Seats:<br>
+<input type="number"><br><br>
+
+Bus Type:<br>
+<select>
+    <option>AC</option>
+    <option>Non-AC</option>
+    <option>Sleeper</option>
+</select><br><br>
+
+<input type="submit" value="Book">
+
+</form>
+
+</body>
+</html>
